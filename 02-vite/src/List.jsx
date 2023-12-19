32c7b2b6cd5dcbb78319ea4132b71bb4f26de347
Matthieu Mota <matthieumota@gmail.com>
2023-12-19 10:18:09 +0100

    state

--- 02-vite/src/List.jsx
+++ 02-vite/src/List.jsx
@@ -7,6 +7,7 @@ function List() {
     { id: 3, name: 'Tata', avatar: 'https://i.pravatar.cc/150?u=Tata' },
   ]);
   const [newName, setNewName] = useState('');
+  const [editing, setEditing] = useState(null);
 
   const addUser = () => {
     let newUser = { id: (users[users.length - 1]?.id ?? 0) + 1, name: newName, avatar: 'https://i.pravatar.cc/150?u=Tata' };
@@ -19,13 +20,33 @@ function List() {
     setUsers(users.filter(u => u.id !== id));
   }
 
+  const editUser = () => {
+    /* setUsers(users.map(user => {
+      if (user.id === editing.id) {
+        return editing;
+      }
+
+      return user;
+    })) */
+
+    setUsers(users.map(user => user.id === editing.id ? editing : user))
+    setEditing(null)
+  }
+
   return (
     <>
       <h2>Utilisateurs ({users.length})</h2>
+      {editing &&
+        <>
+          On édite {JSON.stringify(editing)}
+          <input type="text" value={editing.name} onChange={(e) => setEditing({ ...editing, name: e.target.value })} />
+          <button onClick={editUser}>Modifier</button>
+        </>
+      }
       <ul>
         {users.map(user =>
           <li key={user.id}>
-            <h2>{user.name} ({user.id})</h2>
+            <h2 onClick={() => setEditing(user)}>{user.name} ({user.id})</h2>
             <button onClick={() => deleteUser(user.id)}>❌</button>
           </li>
         )}
