a3d27f0b1e81b5490018e8d28e5b048ef410197c
Matthieu Mota <matthieumota@gmail.com>
2023-12-19 09:53:42 +0100

    wip state

--- 02-vite/src/List.jsx
+++ 02-vite/src/List.jsx
@@ -6,23 +6,32 @@ function List() {
     { id: 2, name: 'Titi', avatar: 'https://i.pravatar.cc/150?u=Titi' },
     { id: 3, name: 'Tata', avatar: 'https://i.pravatar.cc/150?u=Tata' },
   ]);
+  const [newName, setNewName] = useState('');
 
   const addUser = () => {
-    let newUser = { id: 4, name: 'Fiorella', avatar: 'https://i.pravatar.cc/150?u=Tata' };
+    let newUser = { id: (users[users.length - 1]?.id ?? 0) + 1, name: newName, avatar: 'https://i.pravatar.cc/150?u=Tata' };
     setUsers([ ...users, newUser ]); // On fait une copie du tableau
+    setNewName('');
     console.log(users);
+  }
+
+  const deleteUser = (id) => {
+    setUsers(users.filter(u => u.id !== id));
   }
 
   return (
     <>
+      <h2>Utilisateurs ({users.length})</h2>
       <ul>
         {users.map(user =>
           <li key={user.id}>
-            <h2>{user.name}</h2>
+            <h2>{user.name} ({user.id})</h2>
+            <button onClick={() => deleteUser(user.id)}>❌</button>
           </li>
         )}
       </ul>
-      <button onClick={addUser}>Ajouter</button>
+      <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+      <button onClick={addUser} disabled={newName.length === 0}>Ajouter</button>
     </>
   );
 }
